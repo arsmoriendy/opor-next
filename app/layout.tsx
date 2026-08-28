914bcc1a34e6d02259b12b4d0967488fb9f8bd6f
arsmoriendy <nugrahasteven@gmail.com>
2026-08-28 12:01:58 +0700

    chore: init shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist, Geist_Mono, JetBrains_Mono, Lora } from "next/font/google"
 import "./globals.css"
+import { cn } from "@/lib/utils"
+
+const loraHeading = Lora({ subsets: ["latin"], variable: "--font-heading" })
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,8 +29,15 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full
-        antialiased`}
+      className={cn(
+        "h-full",
+        "antialiased",
+        geistSans.variable,
+        geistMono.variable,
+        "font-mono",
+        jetbrainsMono.variable,
+        loraHeading.variable
+      )}
     >
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
