77e3c3162223b751a06a40aeeacee24cac2e7499
arsmoriendy <nugrahasteven@gmail.com>
2026-08-31 16:40:01 +0700

    feat: ports table relation

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,4 +1,5 @@
+import { relations } from "@/lib/db/relations"
 import "@/lib/env-config"
 import { drizzle } from "drizzle-orm/node-postgres"
 
-export const db = drizzle(process.env.DATABASE_URL!)
+export const db = drizzle(process.env.DATABASE_URL!, { relations })
---
+++ lib/db/relations.ts
@@ -0,0 +1,14 @@
+import { defineRelations } from "drizzle-orm"
+import { portsTable, servicesTable } from "@/lib/db/schema"
+
+export const relations = defineRelations(
+  { portsTable, servicesTable },
+  (r) => ({
+    portsTable: {
+      service: r.one.servicesTable({
+        from: r.portsTable.serviceId,
+        to: r.servicesTable.id,
+      }),
+    },
+  })
+)
